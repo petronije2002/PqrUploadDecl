a178e3df93baca263a977fd2d9c8559c946dee34
Petar Jovanovic <Petar.Jovanovic@MacBook-Air-van-Petar-Jovanovic.local>
2019-10-11 10:12:17 +0200

    changed env

--- PqrUploadModule/__init__.py
+++ PqrUploadModule/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask  # Import the Flask class
-app = Flask(__name__,template_folder="Templates")    # Create an instance of the class for our use
 from . import sql_settings
 from flask_bootstrap import Bootstrap
 
+
+app = Flask(__name__,template_folder="Templates")    # Create an instance of the class for our use
 app.config['SECRET_KEY'] = 'This is a string 1234567'
 app.config['SQLALCHEMY_DATABASE_URI'] = sql_settings.uri_azure_sql
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
